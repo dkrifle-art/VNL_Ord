13560a6609309773b17cbc65be28d2a2607aae1c
dkrifle-art <dkrifle@icloud.com>
2026-07-07 12:21:34 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE = 'vnl-orders-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-512-maskable.png'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)));
+    await self.clients.claim();
+  })());
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  const url = new URL(req.url);
+
+  // Никогда не перехватываем кросс-доменные запросы (Firebase, CDN и т.п.)
+  if (url.origin !== self.location.origin) return;
+
+  if (req.mode === 'navigate' || req.destination === 'document') {
+    event.respondWith(
+      fetch(req).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(cache => cache.put('./index.html', copy));
+        return res;
+      }).catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(caches.match(req).then(cached => cached || fetch(req)));
+});
